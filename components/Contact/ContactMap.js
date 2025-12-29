e4967cb5e2b6225922d84113b3e2b6c6a9cd870a
Shvm0032 <shvmbutola@gmail.com>
2025-12-29 17:20:19 +0530

    mapp location

--- components/Contact/ContactMap.js
+++ components/Contact/ContactMap.js
@@ -13,7 +13,7 @@ export default function ContactMap() {
       {/* Google Map */}
       <div className="w-full h-[400px] md:h-[400px] overflow-hidden rounded-lg shadow-lg">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14014.869685006846!2d77.2090215!3d28.6139391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd42f865d15f%3A0x78da37b9ffb5cb2f!2sConnaught%20Place%2C%20New%20Delhi%2C%20Delhi%20110001!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4869.602717531469!2d78.0570384271522!3d30.341595478149163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390929d43ab92d57%3A0x128eb697c057ec78!2sParsvnath%20Complex!5e0!3m2!1sen!2sin!4v1767008926784!5m2!1sen!2sin"
           width="100%"
           height="100%"
           allowFullScreen=""
